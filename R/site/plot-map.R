2450dd8479b3ac1f00b07485043d79daf2792a2a
markjrieke <markjrieke@gmail.com>
2024-09-10 21:50:05 -0500

    update national map

--- R/site/plot-map.R
+++ R/site/plot-map.R
@@ -16,23 +16,17 @@
 #'
 #' @param ... unused
 #' @param branch github branch to extract data from. Defaults to `"dev"`.
-plot_map <- function(..., branch = "dev") {
+plot_map <- function(...,
+                     pal = c("#3579ac", "#7cb0d7", "#d3e5f2",
+                             "#f2f2f2",
+                             "#f2d5d5", "#d78080", "#b13737"),
+                     branch = "dev") {
 
   # generate clickable links
   clickables <-
     read_csv(find_document("data/static/electors.csv")) %>%
     select(state) %>%
     mutate(link = glue::glue("window.open(\"{state}.html\")"))
-
-  rating_text <-
-    tibble(rating = pal,
-           rating_text = c("Safe Dem",
-                           "Very likely Dem",
-                           "Likely Dem",
-                           "Uncertain",
-                           "Likely Rep",
-                           "Very likely Rep",
-                           "Safe Rep"))
 
   us_geo <-
     read_rds(find_document("data/tigris/tigris.rds", branch = branch))
@@ -42,37 +36,14 @@ plot_map <- function(..., branch = "dev") {
     filter(run_date == max(run_date)) %>%
     left_join(read_csv(find_document("data/static/electors.csv", branch = branch))) %>%
     drop_na() %>%
-    mutate(rating = case_when(p_win > 0.99 ~ pal[1],
-                              p_win > 0.85 ~ pal[2],
-                              p_win > 0.65 ~ pal[3],
-                              p_win >= 0.35 ~ pal[4],
-                              p_win >= 0.15 ~ pal[5],
-                              p_win >= 0.01 ~ pal[6],
-                              TRUE ~ pal[7]))
+    mutate(rating = map_chr(p_win, ~interpolate_fill(.x, pal)),
+           color = map_chr(p_win, ~interpolate_fill(.x, c(rep("white", 3), "black", rep("white", 3)))))
 
-  ec_summary <-
+  ratings_pal <-
     state_ratings %>%
-    group_by(rating) %>%
-    summarise(electors = sum(electors))
-
-  map_legend <-
-    ec_summary %>%
-    left_join(rating_text) %>%
-    mutate(rating = fct_relevel(rating, pal)) %>%
-    ggplot(aes(x = rating,
-               y = 0)) +
-    geom_point(aes(fill = rating,
-                   color = if_else(rating_text == "Uncertain", "black", "white")),
-               shape = 21,
-               size = 5) +
-    geom_text(aes(label = rating_text),
-              family = "IBM Plex Sans",
-              size = 3.5,
-              vjust = 3) +
-    scale_fill_identity() +
-    scale_color_identity() +
-    theme_void() +
-    expand_limits(x = c(0.5, 7.5))
+    arrange(p_win) %>%
+    distinct(rating) %>%
+    pull(rating)
 
   map_points <-
     state_ratings %>%
@@ -80,20 +51,12 @@ plot_map <- function(..., branch = "dev") {
     left_join(clickables) %>%
     sf::st_as_sf(coords = c("long", "lat"),
                  crs = 4326) %>%
-    mutate(candidate = if_else(p_win >= 0.5, "Harris", "Trump")) %>%
-    left_join(rating_text) %>%
-    mutate(rating_text = str_to_lower(rating_text),
-           rating_text = str_remove_all(rating_text, " dem| rep"),
-           rating_text = if_else(rating_text == "safe", "all but guaranteed", rating_text),
+    mutate(candidate = if_else(p_win >= 0.5, "Harris", "Trump"),
            p_label = if_else(candidate == "Harris", p_win, 1 - p_win),
            p_label = if_else(p_label > 0.99, ">99%", scales::label_percent(accuracy = 1)(p_label)),
            ev_label = if_else(electors == 1, "EV", "EVs"),
-           text_color = case_when(rating_text == "uncertain" ~ "gray",
-                                  candidate == "Harris" ~ pal[1],
-                                  TRUE ~ pal[7]),
-           summary_text = if_else(rating_text == "uncertain",
-                                  glue::glue("<b>{candidate}</b> has a {p_label} chance of winning"),
-                                  glue::glue("<b>{candidate}</b> is {rating_text} to win ({p_label})")),
+           text_color = if_else(candidate == "Harris", pal[1], pal[7]),
+           summary_text = glue::glue("<b>{candidate}</b> has a {p_label} chance of winning"),
            tooltip = glue::glue("<span style='font-family:IBM Plex Sans'><span style='text-align:left;'><b>{state}</b></span><span style='float:right;font-size:12px;'>({electors} {ev_label})</span>",
                                 "<span style='font-size:14px'>{summary_text}</span></span>",
                                 .sep = "<br>"),
@@ -109,7 +72,7 @@ plot_map <- function(..., branch = "dev") {
                         aes(size = electors,
                             fill = rating,
                             onclick = link,
-                            color = if_else(rating == pal[4], "black", "white"),
+                            color = color,
                             tooltip = tooltip,
                             data_id = state),
                         shape = 21) +
@@ -120,10 +83,8 @@ plot_map <- function(..., branch = "dev") {
     theme(legend.position = "none")
 
   map_bar <-
-    ec_summary %>%
-    mutate(rating = fct_relevel(rating, pal[7:1])) %>%
-    arrange(desc(rating)) %>%
-    mutate(label_pos = cumsum(electors) - electors/2) %>%
+    state_ratings %>%
+    mutate(rating = fct_relevel(rating, ratings_pal)) %>%
     ggplot(aes(x = 0,
                y = electors,
                fill = rating)) +
@@ -134,19 +95,28 @@ plot_map <- function(..., branch = "dev") {
                  y = 269,
                  yend = 269,
                  color = "gray60") +
-    geom_text(aes(y = label_pos,
-                  label = electors),
-              family = "IBM Plex Sans",
-              fontface = "bold",
-              size = 4) +
     scale_fill_identity() +
     coord_flip() +
     theme_void() +
     expand_limits(x = c(-1, 1))
 
   map_full <-
-    (map_legend / map_ratings / map_bar) +
-    plot_layout(heights = c(1, 9, 1))
+    (map_ratings / map_bar) +
+    plot_layout(heights = c(9, 1))
+
+}
+
+#' Interpolate between an evenly distributed color palette
+#'
+#' @param x probability that Harris wins in a given state
+#' @param pal color palette to interpolate through
+interpolate_fill <- function(x, pal) {
+
+  color_rgb <- colorRamp(rev(pal))
+  rgb_vals <- color_rgb(x)
+  hex <- rgb(rgb_vals[1], rgb_vals[2], rgb_vals[3], maxColorValue = 255)
+
+  return(hex)
 
 }
 
